272bb1d95630183c634bc6b6908a362be653b203
Fabian Sauter <sauter.fabian@mailbox.org>
2019-03-25 18:51:23 +0100

    Updated main.py for latest changes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from emoji_parser import EmojiParser
 from gen_c_sharp import GenCSharp
 
 if __name__ == "__main__":
-    parser = EmojiParser("https://unicode.org/Public/emoji/12.0/emoji-test.txt")
+    url = "https://unicode.org/Public/emoji/12.0/emoji-test.txt"
+    parser = EmojiParser(url)
     result = parser.parse()
 
-    GenCSharp.gen(result)
+    GenCSharp.gen(result, url)
